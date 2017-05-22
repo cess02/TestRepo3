de2af9f0b352dfa9a5ee2406cc0182c911d163f2
PrincessJoy <princess.yuu02@gmail.com>
2017-05-22 11:00:33 +0800

    changes1

---
+++ TestRepo3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestRepo3.MvcApplication" Language="C#" %>
---
+++ TestRepo3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestRepo3.Startup))]
+namespace TestRepo3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
